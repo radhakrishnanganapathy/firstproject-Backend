e797fc32f84deb8b419add0eda65b0c0cbafee38
radhakrishnanganapathy <radhakrishnanganapathyr@gmail.com>
2023-04-10 16:35:55 +0530

    new update

--- DataBase/db.py
+++ DataBase/db.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from ..config import settings
+from config import settings
 # import config 
 
-engine = create_engine(settings.db_url)
+DATABASE_URL = settings.db_url
+engine = create_engine(DATABASE_URL)
 # engine = create_engine('postgresql://postgres:root123@localhost:5432/myproj')
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
